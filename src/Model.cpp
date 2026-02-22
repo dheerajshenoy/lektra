2e707df7f75f02bc93ac1b9acfcc7f9ed780e0e9
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2026-02-22 23:13:57 +0100

    Lock mutex when writing to m_text_cache

--- src/Model.cpp
+++ src/Model.cpp
@@ -2281,6 +2281,7 @@ Model::buildTextCacheForPages(const std::set<int> &pagenos) noexcept
                 }
             }
 
+            std::lock_guard<std::recursive_mutex> lock(m_page_cache_mutex);
             m_text_cache.put(pageno, std::move(cache));
         }
         fz_always(ctx)
