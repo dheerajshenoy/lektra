ee15350a33023477e0a93e608f7bf1adf6aa7e08
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2026-05-02 20:18:23 +0200

    Add lua execute code block

--- src/lua/Lektra.cpp
+++ src/lua/Lektra.cpp
@@ -1,10 +1,35 @@
 #include "Lektra.hpp"
 
-#include "api.cpp"
-#include "cmd.cpp"
-#include "event.cpp"
-#include "opt.cpp"
-#include "ui.cpp"
+void
+Lektra::executeLuaCode(const QString &code) noexcept
+{
+    if (!m_L)
+        return;
+
+    // Get the output of the Lua code execution
+    lua_pushcfunction(m_L, [](lua_State *L) -> int
+    {
+        const char *output = lua_tostring(L, 1);
+        if (output)
+        {
+            qDebug() << "Lua Output:" << output;
+        }
+        return 0;
+    });
+
+    if (luaL_loadstring(m_L, code.toStdString().c_str()) != LUA_OK)
+    {
+        qWarning() << "Failed to load Lua code:" << lua_tostring(m_L, -1);
+        lua_pop(m_L, 1); // Remove error message from stack
+        return;
+    }
+
+    if (lua_pcall(m_L, 0, 0, -2) != LUA_OK)
+    {
+        qWarning() << "Failed to execute Lua code:" << lua_tostring(m_L, -1);
+        lua_pop(m_L, 1); // Remove error message from stack
+    }
+}
 
 void
 Lektra::initLua() noexcept
@@ -17,9 +42,9 @@ Lektra::initLua() noexcept
 
     // Register functions
     initLuaOpt();
-    initLuaAPI();
     initLuaCmd();
     initLuaUI();
+    initLuaTabs();
     initLuaEventDispatcher();
 
     lua_setglobal(m_L, "lektra");
