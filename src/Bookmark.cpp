693798e9479163a89e5e73be033b439d75c64043
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2026-05-10 01:11:42 +0200

    Create string uuid for bookmarks

--- src/Bookmark.cpp
+++ src/Bookmark.cpp
@@ -1,7 +1,14 @@
 #include "Bookmark.hpp"
 
+#include <QUuid>
+
 Bookmark::Bookmark(const QString &file_path, const PageLocation &location,
-                   const QDateTime &created_at) noexcept
-    : m_file_path(file_path), m_location(location), m_created_at(created_at)
+                   const QDateTime &created_at, BookmarkId id) noexcept
+    : m_file_path(file_path), m_location(location), m_created_at(created_at),
+      m_id(id)
 {
+    if (m_id.isEmpty())
+    {
+        m_id = QUuid::createUuid().toString();
+    }
 }
